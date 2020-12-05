923484214d0d97792eeb38620ace086aebec6279
sle <silvanlevanti@hotmail.com>
2020-12-05 18:06:49 +0100

    Day 2 Part 1

--- Day_2/day2_a.py
+++ Day_2/day2_a.py
@@ -1,7 +1,16 @@
 # Read the list of inputs saved in passwords.txt
+counter = 0
 with open('passwords.txt') as file:
     for pw in file.readlines():
+        # Separate password and policy
         policy, password = pw.split(':')
-        print(policy, password)
+        # Get policy details
+        minReps = int(policy.split()[0].split('-')[0])
+        maxReps = int(policy.split()[0].split('-')[1])
+        letter = policy.split()[1]
+        # Determine if policy holds
+        if int(password.count(letter)) in range(minReps, maxReps+1):
+            counter = counter + 1
 
+print('Passwords matching policy: ', counter)
 
