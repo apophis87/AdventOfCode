ebb28ed09c459d5bfb92f222592916774cb278f6
SLE <silvan.levanti@swisskrono.com>
2020-12-07 12:55:05 +0100

    Day 6 Part 1

--- Day_6/day6_a.py
+++ Day_6/day6_a.py
@@ -1,10 +1,14 @@
 # Evaluating answers
 answers = ''
+sum = 0
 with open('answers.txt') as file:
     for line in file.readlines():
-        if line:
-            # Gather fields in a list
-            answers = answers + line
+        if line != '\n':
+            # Put all the answers of a group in a contiguous string
+            answers = answers + line.split('\n')[0]
         else:
-            print(answers)
+            # Converting the string to a set, evaluating number of elements
+            # in the set and summing it in each loop to previous intermediate sum
+            sum = sum + len(set(answers))
             answers = ''
+print(sum)
